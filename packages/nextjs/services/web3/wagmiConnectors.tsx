f18344e8c38f08fe7111bfcf4bd961145f634bf5
0xstruct <0xstruct@gmail.com>
2023-08-13 21:04:46 +0630

    set enabledChains in wagmiConnectors to Superchains network (base, optimism, zora, etc)

--- packages/nextjs/services/web3/wagmiConnectors.tsx
+++ packages/nextjs/services/web3/wagmiConnectors.tsx
@@ -19,7 +19,15 @@ const configuredNetwork = getTargetNetwork();
 const { onlyLocalBurnerWallet } = scaffoldConfig;
 
 // We always want to have mainnet enabled (ENS resolution, ETH price, etc). But only once.
-const enabledChains = configuredNetwork.id === 1 ? [configuredNetwork] : [configuredNetwork, chains.mainnet];
+//const enabledChains = configuredNetwork.id === 1 ? [configuredNetwork] : [configuredNetwork, chains.mainnet];
+const enabledChains = [
+  configuredNetwork,
+  chains.baseGoerli,
+  chains.optimismGoerli,
+  chains.zoraTestnet,
+  chains.polygonMumbai,
+  chains.mainnet,
+];
 
 /**
  * Chains for the app
